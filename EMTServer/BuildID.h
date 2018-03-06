5bbf008bab79a8e54ead614eba66538ea26678c0
gilgamesh <c706213d-56d4-42c1-a681-27fd4135a504@gilgamesh.cc>
2018-03-05 22:49:55 -0500

    SMTP test

--- EMTServer/BuildID.h
+++ EMTServer/BuildID.h
@@ -4,4 +4,4 @@
 #define VERSION_MAJOR            3
 #define VERSION_MINOR            3
 #define VERSION_REVISION         1
-#define VERSION_BUILD            280
+#define VERSION_BUILD            281
