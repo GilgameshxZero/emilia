33ffcf4b213d45f7e09c9fca9627681f405a6e10
Yang Yan <yangawesome@gmail.com>
2018-03-05 23:08:39 -0500

    SMTP-test-2

--- EMTSMTPServer/Main.cpp
+++ EMTSMTPServer/Main.cpp
@@ -1,5 +1,12 @@
 #include "Main.h"
 
+int onProcessMessage(void *funcParam) {
+	Rain::WSA2RecvParam &recvParam = *reinterpret_cast<Rain::WSA2RecvParam *>(funcParam);
+	std::cout << recvParam.message;
+
+	return 0;
+}
+
 int main() {
 	WSADATA wsaData;
 	struct addrinfo *sAddr;
@@ -19,11 +26,24 @@ int main() {
 
 	SOCKET cSocket;
 	Rain::servAcceptClient(cSocket, lSocket);
+	Rain::WSA2RecvParam recvParam;
+
+	recvParam.bufLen = 1024;
+	recvParam.funcParam = reinterpret_cast<void *>(&recvParam);
+	recvParam.message = new std::string();
+	recvParam.onProcessMessage = onProcessMessage;
+	recvParam.onRecvInit = NULL;
+	recvParam.onRecvEnd = NULL;
+	recvParam.socket = &cSocket;
+
+	CreateThread(NULL, 0, Rain::recvThread, reinterpret_cast<void *>(&recvParam), NULL, NULL);
 
 	std::cout << "Client IP:\t" << Rain::getClientNumIP(cSocket) << "\n";
 
-	std::string message = "220 foo.com Simple Mail Transfer Service Ready";
+	std::string message = "220 emilia-tan.com Simple Mail Transfer Service Ready\r\n\n\n";
 	Rain::sendText(cSocket, message.c_str(), message.length());
+
+	std::cin.get();
 
 	return 0;
 }
