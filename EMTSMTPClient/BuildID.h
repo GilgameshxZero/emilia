c5ef864495f122d2bd555f8fac23fb8d6f465b72
gilgamesh <c706213d-56d4-42c1-a681-27fd4135a504@gilgamesh.cc>
2018-03-07 08:33:56 -0500

    .

--- EMTSMTPClient/BuildID.h
+++ EMTSMTPClient/BuildID.h
@@ -4,4 +4,4 @@
 #define VERSION_MAJOR            1
 #define VERSION_MINOR            0
 #define VERSION_REVISION         0
-#define VERSION_BUILD            107
+#define VERSION_BUILD            108
