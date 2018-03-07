8eec11a594fdc75ae7a370a43abcb191910724b4
gilgamesh <c706213d-56d4-42c1-a681-27fd4135a504@gilgamesh.cc>
2018-03-07 10:09:30 -0500

    client refactor

--- EMTSMTPClient/RecvThreadHandlers.cpp
+++ EMTSMTPClient/RecvThreadHandlers.cpp
@@ -6,54 +6,67 @@ namespace Mono3 {
 			RecvThreadParam &rtParam = *reinterpret_cast<RecvThreadParam *>(funcParam);
 			std::map<std::string, std::string> &config = *rtParam.config;
 
-			//send response based on state
 			std::stringstream response;
-			if (rtParam.state == "HELO") {
-				rtParam.state = "FROM";
-				response << "EHLO " << config["ehloResponse"] << "\r\n";
-			} else if (rtParam.state == "FROM") {
-				rtParam.state = "TO";
-				response << "MAIL FROM:<" << config["fromEmail"] << ">\r\n";
-			} else if (rtParam.state == "TO") {
-				rtParam.state = "DATA";
-				response << "RCPT TO:<" << config["toEmail"] << ">\r\n";
-			} else if (rtParam.state == "DATA") {
-				rtParam.state = "actualData";
-				response << "DATA\r\n";
-			} else if (rtParam.state == "actualData") {
-				rtParam.state = "QUIT";
-				std::string emailBody;
-				response << "MIME-Version: 1.0\r\n"
-					<< "DKIM-Signature: v=1; a=rsa-sha256; s=dkim; d=emilia-tan.com; h=Received : From : To : Subject : ; l=0; bh=;"
-					<< "From: " << config["fromName"] << " <" << config["fromEmail"] << ">\r\n"
-					<< "Subject: " << config["emailSubject"] << "\r\n"
-					<< "To: " << config["toEmail"] << "\r\n"
-					<< "Content-Type: text/plain; charset=\"UTF-8\"\r\n"
-					<< "\r\n"
-					<< Rain::readFullFile(config["emailBody"], emailBody) << "\r\n"
-					<< ".\r\n";
-			} else if (rtParam.state == "QUIT") {
-				rtParam.state = "finished";
-				response << "QUIT\r\n";
-			} else if (rtParam.state == "finished")
-				return 1;
+			rtParam.smtpWaitFunc(rtParam, config, rtParam.message, response);
 			Rain::sendText(*rtParam.sSocket, response.str());
 
-			//logging
 			std::cout << rtParam.message << response.str();
-			std::string logData = rtParam.message + response.str();
-			Rain::fastOutputFile(config["logFile"], logData, true);
+			Rain::fastOutputFile(config["logFile"], rtParam.message + response.str(), true);
 
 			return 0;
 		}
 		void onRecvInit(void *funcParam) {
 			RecvThreadParam &rtParam = *reinterpret_cast<RecvThreadParam *>(funcParam);
-			rtParam.state = "HELO";
 			rtParam.mainMutex.lock();
+			rtParam.smtpWaitFunc = waitEhlo;
 		}
 		void onRecvEnd(void *funcParam) {
 			RecvThreadParam &rtParam = *reinterpret_cast<RecvThreadParam *>(funcParam);
 			rtParam.mainMutex.unlock();
+			rtParam.socketActive = false;
+		}
+
+		int waitEhlo(RecvThreadParam &rtParam, std::map<std::string, std::string> &config, std::string &message, std::stringstream &response) {
+			response << "EHLO " << config["ehloResponse"] << "\r\n";
+			rtParam.smtpWaitFunc = waitMailFrom;
+			return 0;
+		}
+		int waitMailFrom(RecvThreadParam &rtParam, std::map<std::string, std::string> &config, std::string &message, std::stringstream &response) {
+			response << "MAIL FROM:<" << config["fromEmail"] << ">\r\n";
+			rtParam.smtpWaitFunc = waitRcptTo;
+			return 0;
+		}
+		int waitRcptTo(RecvThreadParam &rtParam, std::map<std::string, std::string> &config, std::string &message, std::stringstream &response) {
+			response << "RCPT TO:<" << config["toEmail"] << ">\r\n";
+			rtParam.smtpWaitFunc = waitData;
+			return 0;
+		}
+		int waitData(RecvThreadParam &rtParam, std::map<std::string, std::string> &config, std::string &message, std::stringstream &response) {
+			response << "DATA\r\n";
+			rtParam.smtpWaitFunc = waitSendMail;
+			return 0;
+		}
+		int waitSendMail(RecvThreadParam &rtParam, std::map<std::string, std::string> &config, std::string &message, std::stringstream &response) {
+			std::string emailBody;
+			response << "MIME-Version: 1.0\r\n"
+				<< "DKIM-Signature: v=1; a=rsa-sha256; s=dkim; d=emilia-tan.com; h=Received : From : To : Subject : ; l=0; bh=;"
+				<< "From: " << config["fromName"] << " <" << config["fromEmail"] << ">\r\n"
+				<< "Subject: " << config["emailSubject"] << "\r\n"
+				<< "To: " << config["toEmail"] << "\r\n"
+				<< "Content-Type: text/plain; charset=\"UTF-8\"\r\n"
+				<< "\r\n"
+				<< Rain::readFullFile(config["emailBody"], emailBody) << "\r\n"
+				<< ".\r\n";
+			rtParam.smtpWaitFunc = waitQuit;
+			return 0;
+		}
+		int waitQuit(RecvThreadParam &rtParam, std::map<std::string, std::string> &config, std::string &message, std::stringstream &response) {
+			response << "QUIT\r\n";
+			rtParam.smtpWaitFunc = waitEnd;
+			return 0;
+		}
+		int waitEnd(RecvThreadParam &rtParam, std::map<std::string, std::string> &config, std::string &message, std::stringstream &response) {
+			return 1;
 		}
 	}
 }
--- EMTSMTPClient/RecvThreadHandlers.h
+++ EMTSMTPClient/RecvThreadHandlers.h
@@ -7,5 +7,13 @@ namespace Mono3 {
 		int onProcessMessage(void *funcParam);
 		void onRecvInit(void *funcParam);
 		void onRecvEnd(void *funcParam);
+
+		int waitEhlo(RecvThreadParam &rtParam, std::map<std::string, std::string> &config, std::string &message, std::stringstream &response);
+		int waitMailFrom(RecvThreadParam &rtParam, std::map<std::string, std::string> &config, std::string &message, std::stringstream &response);
+		int waitRcptTo(RecvThreadParam &rtParam, std::map<std::string, std::string> &config, std::string &message, std::stringstream &response);
+		int waitData(RecvThreadParam &rtParam, std::map<std::string, std::string> &config, std::string &message, std::stringstream &response);
+		int waitSendMail(RecvThreadParam &rtParam, std::map<std::string, std::string> &config, std::string &message, std::stringstream &response);
+		int waitQuit(RecvThreadParam &rtParam, std::map<std::string, std::string> &config, std::string &message, std::stringstream &response);
+		int waitEnd(RecvThreadParam &rtParam, std::map<std::string, std::string> &config, std::string &message, std::stringstream &response);
 	}
 }
