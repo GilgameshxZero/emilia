01a5ef24c6e2d40ad100e7fbe6b642dd786b2f15
Yang Yan <yangawesome@gmail.com>
2018-03-07 23:05:56 -0500

    builds

--- EMTServer/BuildID.h
+++ EMTServer/BuildID.h
@@ -4,4 +4,4 @@
 #define VERSION_MAJOR            3
 #define VERSION_MINOR            4
 #define VERSION_REVISION         0
-#define VERSION_BUILD            283
+#define VERSION_BUILD            287
