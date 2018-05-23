4ced7fc198636e9173d72c0eac82c114db58114e
gilgamesh <c706213d-56d4-42c1-a681-27fd4135a504@gilgamesh.cc>
2018-05-23 15:41:43 -0400

    .

--- Development/Code/Common/RainAeternum/UtilityLibraries.h
+++ Development/Code/Common/RainAeternum/UtilityLibraries.h
@@ -8,10 +8,24 @@ Include this for all UtilityLibraries libraries.
 
 #pragma once
 
+#include "UtilityError.h"
 #include "UtilityFilesystem.h"
+#include "UtilityLogging.h"
 #include "UtilityString.h"
 #include "UtilityTime.h"
 
 namespace Rain {
 	HANDLE simpleCreateThread(LPTHREAD_START_ROUTINE threadfunc, LPVOID threadparam);
+
+	//concatenates one map into another, shorthand
+	template <class T1, class T2>
+	std::map<T1, T2> &concatMap(std::map<T1, T2> &m1, std::map<T1, T2> m2) {
+		m1.insert(m2.begin(), m2.end());
+		return m1;
+	}
+	template <class T1, class T2>
+	std::map<T1, T2> &concatMap(std::map<T1, T2> &m1, std::map<T1, T2> *m2) {
+		m1.insert(m2->begin(), m2->end());
+		return m1;
+	}
 }
