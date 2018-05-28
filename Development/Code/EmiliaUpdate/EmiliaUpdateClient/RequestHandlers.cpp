f0f4c11893d55ff22f2506d3b04268b36c5838d1
gilgamesh <c706213d-56d4-42c1-a681-27fd4135a504@gilgamesh.cc>
2018-05-28 07:54:46 -0600

    .

--- Development/Code/EmiliaUpdate/EmiliaUpdateClient/RequestHandlers.cpp
+++
@@ -1,9 +0,0 @@
-#include "RequestHandlers.h"
-
-namespace Monochrome3 {
-	namespace EmiliaUpdateServer {
-		int HandleRequest(Rain::ServerSocketManager::ServerSocketManagerDelegateHandlerParam &ssmdhParam) {
-			return 0;
-		}
-	}
-}
