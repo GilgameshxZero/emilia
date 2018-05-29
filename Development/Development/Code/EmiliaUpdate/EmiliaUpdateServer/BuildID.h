3ae05c7ba4abb678b958132ed39df80c7d1261c1
Yang Yan <yangawesome@gmail.com>
2018-05-29 13:02:43 -0600

    1.1.4-debug

--- Development/Development/Code/EmiliaUpdate/EmiliaUpdateServer/BuildID.h
+++ Development/Development/Code/EmiliaUpdate/EmiliaUpdateServer/BuildID.h
@@ -4,4 +4,4 @@
 #define VERSION_MAJOR            1
 #define VERSION_MINOR            0
 #define VERSION_REVISION         1
-#define VERSION_BUILD            124
+#define VERSION_BUILD            125
