3fc8125b9146a17c6868b92ffdc1ad7220fa66f3
gilgamesh <c706213d-56d4-42c1-a681-27fd4135a504@gilgamesh.cc>
2018-12-21 20:20:23 -0700

    5.4.3

--- rc/build.h
+++ rc/build.h
@@ -3,5 +3,5 @@
 #define STRINGIZE(s) STRINGIZE2(s)
 #define VERSION_MAJOR            5
 #define VERSION_MINOR            4
-#define VERSION_REVISION         2
-#define VERSION_BUILD            1888
+#define VERSION_REVISION         3
+#define VERSION_BUILD            1890
