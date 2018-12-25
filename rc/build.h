09e99fa909962e91ed85e84ce0bd6b643afc89d8
Yang Yan <yangawesome@gmail.com>
2018-12-24 20:39:39 -0700

    5.6.0

--- rc/build.h
+++ rc/build.h
@@ -2,6 +2,6 @@
 #define STRINGIZE2(s) #s
 #define STRINGIZE(s) STRINGIZE2(s)
 #define VERSION_MAJOR            5
-#define VERSION_MINOR            5
+#define VERSION_MINOR            6
 #define VERSION_REVISION         0
-#define VERSION_BUILD            1929
+#define VERSION_BUILD            1946
