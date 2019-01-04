5c039344569febba4437d47308301aeaf4a75a10
gilgamesh <c706213d-56d4-42c1-a681-27fd4135a504@gilgamesh.cc>
2019-01-04 15:35:00 -0500

    5.7.1

--- rc/build.h
+++ rc/build.h
@@ -3,5 +3,5 @@
 #define STRINGIZE(s) STRINGIZE2(s)
 #define VERSION_MAJOR            5
 #define VERSION_MINOR            7
-#define VERSION_REVISION         0
-#define VERSION_BUILD            2033
+#define VERSION_REVISION         1
+#define VERSION_BUILD            2036
