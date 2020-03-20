649c2eb0f128ddfecece0d854cc7830b5abb7b18
gilgamesh <c706213d-56d4-42c1-a681-27fd4135a504@gilgamesh.cc>
2020-03-20 13:01:55 -0600

    6.0.11

--- rc/build.h
+++ rc/build.h
@@ -3,5 +3,5 @@
 #define STRINGIZE(s) STRINGIZE2(s)
 #define VERSION_MAJOR            6
 #define VERSION_MINOR            0
-#define VERSION_REVISION         9
-#define VERSION_BUILD            2520
+#define VERSION_REVISION         11
+#define VERSION_BUILD            2549
---
+++ src/constants.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include "../rain/cpp/rain.hpp"
+
+namespace Emilia {
+	const enum class CMD_OPTION_R { NONE, CRASH, DEPLOY };
+	extern const int MAX_PROJECT_DIR_SEARCH;
+	extern const std::string RESTART_SHELL_SCRIPT,
+		DEFAULT_CONFIGURATION,
+		PROJECT_DIR,
+		PROJECT_INDEX,
+		PROJECT_CONFIG;
+}
