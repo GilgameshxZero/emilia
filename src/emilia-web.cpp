f779adcbf53d4115391e9bb002b411f24a8d32bf
gilgamesh <c706213d-56d4-42c1-a681-27fd4135a504@gilgamesh.cc>
2020-07-04 16:16:41 -0600

    7.0.6

--- src/emilia-web.cpp
+++ src/emilia-web.cpp
@@ -4,14 +4,15 @@
 
 int main(int argc, char *argv[], char **envp) {
 	Rain::HttpServer server;
-	server.onNew = [](Rain::HttpServerSlave *socket) {
-		std::cout << "onNew" << std::endl;
-	};
-	server.onClose = [](Rain::HttpServerSlave *socket) {
-		std::cout << "onClose" << std::endl;
+	server.onRequest = [](Rain::HttpRequest *req) {
+		std::cout << req->method << " " << req->uri << " HTTP/" << req->version
+							<< std::endl;
+		for (auto it = req->headers.begin(); it != req->headers.end(); it++) {
+			std::cout << it->first << ": " << it->second << std::endl;
+		}
 	};
 	std::cout << "Starting server: " << server.serve(false) << std::endl;
-	Rain::sleep(10000);
+	Rain::sleep(60000);
 	std::cout << "Stopping server: " << server.close() << std::endl;
 
 	Rain::Socket::cleanup();
