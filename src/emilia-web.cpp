5d43432ec2b3b9b7292fc3f3e5a178420c93a843
GILGAMESH <~@gilgamesh.cc>
2020-07-04 12:33:25 -0600

    7.0.5

--- src/emilia-web.cpp
+++ src/emilia-web.cpp
@@ -1,9 +1,18 @@
 #include "emilia-web.hpp"
 
+#include <iostream>
+
 int main(int argc, char *argv[], char **envp) {
-	Rain::ThreadPool threadPool;
-	Rain::HttpServerSocket socket;
-	socket.serve("80", &threadPool);
+	Rain::HttpServer server;
+	server.onNew = [](Rain::HttpServerSlave *socket) {
+		std::cout << "onNew" << std::endl;
+	};
+	server.onClose = [](Rain::HttpServerSlave *socket) {
+		std::cout << "onClose" << std::endl;
+	};
+	std::cout << "Starting server: " << server.serve(false) << std::endl;
+	Rain::sleep(10000);
+	std::cout << "Stopping server: " << server.close() << std::endl;
 
 	Rain::Socket::cleanup();
 	return 0;
