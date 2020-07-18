30da7d497d5234c83756ed76a979de7d261de237
GILGAMESH <~@gilgamesh.cc>
2020-07-18 16:12:34 -0600

    7.0.9

--- include/emilia-web.hpp
+++ include/emilia-web.hpp
@@ -2,10 +2,10 @@
 
 #define EMILIA_WEB_VERSION_MAJOR 7
 #define EMILIA_WEB_VERSION_MINOR 0
-#define EMILIA_WEB_VERSION_REVISION 8
+#define EMILIA_WEB_VERSION_REVISION 9
 
 #include "build.hpp"
 
 #include <rain.hpp>
 
-int main(int, char *[], char **);
+int main(int, const char *[]);
