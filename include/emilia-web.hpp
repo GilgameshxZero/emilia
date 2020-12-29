649774461ee83dcdcf103f607326047b493748eb
GILGAMESH <~@gilgamesh.cc>
2020-12-29 09:50:37 -0700

    7.1.2

--- include/emilia-web.hpp
+++ include/emilia-web.hpp
@@ -2,33 +2,16 @@
 
 #define EMILIA_WEB_VERSION_MAJOR 7
 #define EMILIA_WEB_VERSION_MINOR 1
-#define EMILIA_WEB_VERSION_REVISION 1
+#define EMILIA_WEB_VERSION_REVISION 2
 
 #include "build.hpp"
 
 #include <rain.hpp>
 
-class Server;
-
-class ServerSlave
-		: public Rain::Networking::Http::ServerSlave<Server, ServerSlave, void *> {
-	public:
-	typedef Rain::Networking::Http::ServerSlave<Server, ServerSlave, void *>
-		ServerSlaveBase;
-	ServerSlave(Rain::Networking::Socket &socket, Server *server)
-			: Rain::Networking::Socket(std::move(socket)),
-				ServerSlaveBase(socket, server) {}
-};
-
-class Server : public Rain::Networking::Http::Server<ServerSlave> {
-	public:
-	typedef Rain::Networking::Http::Server<ServerSlave> ServerBase;
-	Server(std::size_t maxThreads = 0, std::size_t slaveBufSz = 16384)
-			: ServerBase(maxThreads, slaveBufSz) {}
-
+class Server
+		: public Rain::Networking::Http::Server<Rain::Networking::Http::Slave> {
 	protected:
-	void *getSubclassPtr() { return reinterpret_cast<void *>(this); }
-	void onRequest(Request *);
+	bool onRequest(Slave &slave, Request &req) noexcept override;
 };
 
 int main(int, const char *[]);
