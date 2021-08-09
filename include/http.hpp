af210f28e3b1d01932b006384bd46b0fa5e33079
GILGAMESH <~@gilgamesh.cc>
2021-08-09 17:58:38 +0000

    8.0.2

--- include/http.hpp
+++ include/http.hpp
@@ -6,8 +6,30 @@
 namespace Emilia::Http {
 	class Worker final : public Rain::Networking::Http::WorkerInterface<
 												 Rain::Networking::Http::Socket> {
+		private:
+		std::list<std::tuple<
+			std::chrono::system_clock::time_point,
+			bool,
+			Rain::Networking::Smtp::Mailbox,
+			Rain::Networking::Smtp::Mailbox>> &mailboxActivity;
+		std::mutex &mailboxActivityMtx;
+
 		public:
-		using Interface::WorkerInterface;
+		Worker(
+			Rain::Networking::Resolve::AddressInfo const &,
+			Rain::Networking::Socket &&,
+			std::size_t = std::size_t(1) << 10,
+			std::size_t = std::size_t(1) << 10,
+			Duration = std::chrono::seconds(60),
+			Duration = std::chrono::seconds(60),
+			std::list<std::tuple<
+				std::chrono::system_clock::time_point,
+				bool,
+				Rain::Networking::Smtp::Mailbox,
+				Rain::Networking::Smtp::Mailbox>> * = nullptr,
+			std::mutex * = nullptr);
+		Worker(Worker const &) = delete;
+		Worker &operator=(Worker const &) = delete;
 
 		private:
 		// Extend target match chain.
@@ -19,7 +41,40 @@ namespace Emilia::Http {
 		PreResponse getAll(Request &, std::smatch const &);
 	};
 
-	typedef Rain::Networking::Http::
-		ServerInterface<Rain::Networking::Http::Socket, Worker>
-			Server;
+	class Server : public Rain::Networking::Http::
+									 ServerInterface<Rain::Networking::Http::Socket, Worker> {
+		friend Worker;
+
+		private:
+		std::list<std::tuple<
+			std::chrono::system_clock::time_point,
+			bool,
+			Rain::Networking::Smtp::Mailbox,
+			Rain::Networking::Smtp::Mailbox>> &mailboxActivity;
+		std::mutex &mailboxActivityMtx;
+
+		public:
+		Server(
+			std::size_t = 1024,
+			Rain::Networking::Specification::ProtocolFamily =
+				Rain::Networking::Specification::ProtocolFamily::INET6,
+			std::size_t = std::size_t(1) << 10,
+			std::size_t = std::size_t(1) << 10,
+			Duration = std::chrono::seconds(60),
+			Duration = std::chrono::seconds(60),
+			std::list<std::tuple<
+				std::chrono::system_clock::time_point,
+				bool,
+				Rain::Networking::Smtp::Mailbox,
+				Rain::Networking::Smtp::Mailbox>> * = nullptr,
+			std::mutex * = nullptr);
+		Server(Server const &) = delete;
+		Server &operator=(Server const &) = delete;
+
+		private:
+		virtual std::unique_ptr<Worker> workerFactory(
+			std::shared_ptr<std::pair<
+				Rain::Networking::Socket,
+				Rain::Networking::Resolve::AddressInfo>> acceptRes) override;
+	};
 }
--- src/http.cpp
+++ src/http.cpp
@@ -7,6 +7,29 @@ namespace Emilia::Http {
 	using namespace Rain::Literal;
 	using namespace Rain::Networking::Http;
 
+	Worker::Worker(
+		Rain::Networking::Resolve::AddressInfo const &addressInfo,
+		Rain::Networking::Socket &&socket,
+		std::size_t recvBufferLen,
+		std::size_t sendBufferLen,
+		Duration maxRecvIdleDuration,
+		Duration sendOnceTimeoutDuration,
+		std::list<std::tuple<
+			std::chrono::system_clock::time_point,
+			bool,
+			Rain::Networking::Smtp::Mailbox,
+			Rain::Networking::Smtp::Mailbox>> *mailboxActivity,
+		std::mutex *mailboxActivityMtx)
+			: Interface(
+					addressInfo,
+					std::move(socket),
+					recvBufferLen,
+					sendBufferLen,
+					maxRecvIdleDuration,
+					sendOnceTimeoutDuration),
+				mailboxActivity(*mailboxActivity),
+				mailboxActivityMtx(*mailboxActivityMtx) {}
+
 	std::optional<Worker::PreResponse> Worker::chainMatchTargetImpl(
 		Tag<Interface>,
 		Request &req) {
@@ -29,8 +52,8 @@ namespace Emilia::Http {
 							this->addressInfo.address, this->addressInfo.addressLen)
 				 << ".\n"
 				 << "This website is under construction. Please visit later.\n"
-				 << "You may reach me at any mailbox under this domain, except for "
-						"robots@gilgamesh.cc.\n"
+				 << "You may reach me at any RFC3696-valid local-part under this "
+						"domain, except for canned-pork@gilgamesh.cc.\n"
 				 << "\n"
 				 << "Some links:\n"
 				 << "* https://3therflux.bandcamp.com/album/prismaticism\n"
@@ -41,7 +64,24 @@ namespace Emilia::Http {
 				 << "\n"
 				 << "Your request and headers:\n"
 				 << req.method << " " << req.target << " HTTP/" << req.version << "\n"
-				 << req.headers;
+				 << req.headers << "\n"
+				 << "SMTP activity since server restart:\n";
+
+		for (auto it = this->mailboxActivity.rbegin();
+				 it != this->mailboxActivity.rend();
+				 it++) {
+			std::time_t time = std::chrono::system_clock::to_time_t(std::get<0>(*it));
+			std::tm timeData;
+			Rain::Time::localtime_r(&time, &timeData);
+			Rain::Networking::Smtp::Mailbox &from(std::get<2>(*it)),
+				&to(std::get<3>(*it));
+			body << std::put_time(&timeData, "%F %T %z") << " | "
+					 << (std::get<1>(*it) ? "Success" : "Failure") << " | "
+					 << std::string(from.name.length(), '.') << "@" << from.domain
+					 << " > " << std::string(to.name.length(), '.') << "@" << to.domain
+					 << "\n";
+		}
+
 		return {
 			StatusCode::OK,
 			{},
@@ -49,4 +89,43 @@ namespace Emilia::Http {
 			"Why are you looking here?",
 			req.version};
 	}
+
+	Server::Server(
+		std::size_t maxThreads,
+		Rain::Networking::Specification::ProtocolFamily pf,
+		std::size_t recvBufferLen,
+		std::size_t sendBufferLen,
+		Duration maxRecvIdleDuration,
+		Duration sendOnceTimeoutDuration,
+		std::list<std::tuple<
+			std::chrono::system_clock::time_point,
+			bool,
+			Rain::Networking::Smtp::Mailbox,
+			Rain::Networking::Smtp::Mailbox>> *mailboxActivity,
+		std::mutex *mailboxActivityMtx)
+			: Interface(
+					maxThreads,
+					// Relay worker construction arguments.
+					pf,
+					recvBufferLen,
+					sendBufferLen,
+					maxRecvIdleDuration,
+					sendOnceTimeoutDuration),
+				mailboxActivity(*mailboxActivity),
+				mailboxActivityMtx(*mailboxActivityMtx) {}
+
+	std::unique_ptr<Worker> Server::workerFactory(
+		std::shared_ptr<std::pair<
+			Rain::Networking::Socket,
+			Rain::Networking::Resolve::AddressInfo>> acceptRes) {
+		return std::make_unique<Worker>(
+			acceptRes->second,
+			std::move(acceptRes->first),
+			this->recvBufferLen,
+			this->sendBufferLen,
+			this->maxRecvIdleDuration,
+			this->sendOnceTimeoutDuration,
+			&this->mailboxActivity,
+			&this->mailboxActivityMtx);
+	};
 }
