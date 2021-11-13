3dfcfc787b65552e3105fbb78c0f2c2ab72e9789
gilgamesh <c706213d-56d4-42c1-a681-27fd4135a504@gilgamesh.cc>
2021-11-13 13:39:21 -0700

    8.2.0

--- build/.gitignore
+++
@@ -1,1 +0,0 @@
-vcvarsall.bat
--- include/http.hpp
+++ include/http.hpp
@@ -1,11 +1,13 @@
 // Public interfaces for http.cpp.
 #pragma once
 
-#include "state.hpp"
+#include "smtp.hpp"
 
 #include <rain.hpp>
 
 namespace Emilia::Http {
+	class Server;
+
 	class Worker : public Rain::Networking::Http::Worker<
 									 Rain::Networking::Http::Request,
 									 Rain::Networking::Http::Response,
@@ -30,28 +32,33 @@ namespace Emilia::Http {
 			Rain::Networking::TcpProtocolInterface,
 			Rain::Networking::NoLingerSocketOption>;
 
-		State &state;
-
-		// Load from cache or load from filesystem.
-		ResponseAction staticFile(
-			std::filesystem::path const &,
-			std::string const &);
-
-		ResponseAction reqStatus(Request &, std::smatch const &);
-		ResponseAction reqHyperspace(Request &, std::smatch const &);
-		ResponseAction reqHyperpanel(Request &, std::smatch const &);
-		ResponseAction reqPastel(Request &, std::smatch const &);
-		ResponseAction reqStarfall(Request &, std::smatch const &);
-		ResponseAction reqEutopia(Request &, std::smatch const &);
-		virtual std::vector<RequestFilter> filters() override;
-
 		public:
-		Worker(NativeSocket, SocketInterface *, State &);
+		Worker(NativeSocket, SocketInterface *, Server &);
 
 		using SuperWorker::send;
 		using SuperWorker::recv;
 		virtual void send(Response &) override;
 		virtual Request &recv(Request &) override;
+
+		private:
+		// Server state.
+		Server &server;
+
+		virtual std::vector<RequestFilter> filters() override;
+		ResponseAction request(Request &, std::smatch const &);
+
+		// Resolves a path + theme into a filesystem path based on precedence.
+		// Any path returned is a valid file.
+		std::optional<std::filesystem::path> resolveThemedPath(
+			std::string const &,
+			std::string const &);
+
+		// Make a response from a static file at a path. Assumes path is resolved
+		// and valid.
+		ResponseAction requestStatic(std::filesystem::path const &);
+
+		// Server status.
+		ResponseAction requestStatus(Request &, std::smatch const &);
 	};
 
 	class Server : public Rain::Networking::Http::Server<
@@ -61,6 +68,9 @@ namespace Emilia::Http {
 									 Rain::Networking::TcpProtocolInterface,
 									 Rain::Networking::DualStackSocketOption,
 									 Rain::Networking::NoLingerSocketOption> {
+		// Allow state access.
+		friend Worker;
+
 		private:
 		using SuperServer = Rain::Networking::Http::Server<
 			Worker,
@@ -70,10 +80,17 @@ namespace Emilia::Http {
 			Rain::Networking::DualStackSocketOption,
 			Rain::Networking::NoLingerSocketOption>;
 
-		State &state;
+		// State.
+		std::tm const &processBegin;
+		std::unique_ptr<Emilia::Smtp::Server> &smtpServer;
+		std::atomic_bool const &echo;
 
 		public:
-		Server(State &, Host const &);
+		Server(
+			Host const &,
+			std::tm const &,
+			std::unique_ptr<Emilia::Smtp::Server> &,
+			std::atomic_bool const &);
 		virtual ~Server();
 
 		private:
