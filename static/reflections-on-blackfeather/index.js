9eae7936043f04df41bbaf8a47550ea9163ef1d2
GILGAMESH <~@gilgamesh.cc>
2021-12-30 19:17:20 +0000

    8.3.1

--- static/reflections-on-blackfeather/index.js
+++ static/reflections-on-blackfeather/index.js
@@ -5,9 +5,25 @@ import "./components/underlay.js";
 window.addEventListener(
 	`load`,
 	() => {
-		// Wait for all immediately accessible components to load.
 		const underlay = document.querySelector(`emilia-underlay`);
+
+		let locationName;
+		if (window.location.pathname.match(/\/storyworlds\/?/g)) {
+			locationName = `arx`;
+		} else if (window.location.pathname.match(/\/snapshots\/[^\/\?#]+/g)) {
+			locationName = `blackfeather`;
+		} else {
+			locationName = `coast`;
+		}
+		underlay.setAttribute(`location`, locationName);
+
+		// Wait for all immediately accessible components to load.
 		Promise.all([underlay.componentLoad]).then(() => {
+			if (locationName === `blackfeather`) {
+				underlay.toBlackfeather(
+					window.location.pathname.match(/\/snapshots\/([^\/\?#]+)/)[1]
+				);
+			}
 			document.body.removeAttribute(`pre-load`);
 		});
 	},
