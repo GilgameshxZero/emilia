063925f4d1efd97fc58e7f4157d5e79124bc1d8a
gilgamesh <c706213d-56d4-42c1-a681-27fd4135a504@gilgamesh.cc>
2026-02-23 05:12:09 +0000

    unified makefile

--- build/make.bat
+++ build/make.bat
@@ -1,15 +1,28 @@
-@REM Proxy file that calls nmake with INCL set to all header directories.
+@REM Proxy file that calls `nmake` with evaluated wildcards.
 @ECHO OFF
+
+@REM Inject `nmake` with `vcvars` if not yet available.
+WHERE nmake >NUL 2>NUL
+IF %ERRORLEVEL% NEQ 0 (
+	CALL "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+)
+
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-@REM Careful to not exceed the 8192 string limit.
-SET "INCL_PCH=..\rain\include\*"
+@REM Pre-resolve a few wildcard variables before `nmake`.
+@REM Careful to not exceed the 8192 string limit. We do not
+@REM explicitly list `*.hpp` files here for that reason.
+SET "NMAKE_INCL_PCH=..\rain\include\*"
 FOR /F "delims=" %%I IN ('DIR /B /S /AD ..\rain\include') DO (
-	SET "INCL_PCH=%%I\* !INCL_PCH!"
+	SET "NMAKE_INCL_PCH=!NMAKE_INCL_PCH! %%I\*"
 )
-SET "INCL=%INCL_PCH% ..\include\*"
+SET "NMAKE_INCL=..\include\*"
 FOR /F "delims=" %%I IN ('DIR /B /S /AD ..\include') DO (
-	SET "INCL=%%I\* !INCL!"
+	SET "NMAKE_INCL=!NMAKE_INCL! %%I\*"
+)
+SET "NMAKE_PROJ_SRC=..\src\*.cpp"
+FOR /F "delims=" %%I IN ('DIR /B /S /AD ..\src') DO (
+	SET "NMAKE_PROJ_SRC=!NMAKE_PROJ_SRC! %%I\*.cpp"
 )
 
 @REM Suppresses CMD terminate prompt and error code.
