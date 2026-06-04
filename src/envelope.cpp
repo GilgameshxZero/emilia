8583f07c716f5866102674779f84a07602ae1965
gilgamesh <c706213d-56d4-42c1-a681-27fd4135a504@gilgamesh.cc>
2026-06-04 17:48:29 +0000

    massively increase retry attempts to 3 days every 30 minutes

--- src/envelope.cpp
+++ src/envelope.cpp
@@ -3,8 +3,8 @@
 #include <envelope.hpp>
 
 namespace Emilia {
-	std::chrono::minutes const Envelope::RETRY_WAIT(15);
-	std::size_t const Envelope::ATTEMPTS_MAX{8};
+	std::chrono::minutes const Envelope::RETRY_WAIT(30);
+	std::size_t const Envelope::ATTEMPTS_MAX{144};
 
 	Envelope::Envelope(
 		Status status,
